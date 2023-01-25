87cb109d823ebf0245e50615a993aef01dac311f
ackatz <andrew@akatz.org>
2023-01-25 09:30:15 -0800

    first commit

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Bring down SQLite Database
-litestream restore -o /app/rss.db s3://ls-cyberfeed
+litestream restore -o /app/workout_data.db s3://ls-peloapp
 
 litestream replicate -exec "uvicorn app.main:app --host 0.0.0.0 --port 8000" & supercronic /app/crontab
